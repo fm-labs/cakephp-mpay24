9da23562b4b058668766fd3aaf7bec7cc033922d
fm-labs <flowmotionlabs@gmail.com>
2023-03-27 22:23:36 +0200

    Fixed: Mpay24 testsystem mode must be read from profile. The config key 'Mpay24.useTestSystem' has been removed.

--- config/mpay24.php
+++ config/mpay24.php
@@ -1,18 +1,19 @@
 <?php
 return [
     'Mpay24' => [
-        'useTestSystem' => false,
 
         'production' => [
             'merchantId' => null,
             'merchantPassword' => null,
             'debug' => true,
+            'useTestSystem' => false,
         ],
 
         'testing' => [
             'merchantId' => null,
             'merchantPassword' => null,
             'debug' => true,
+            'useTestSystem' => true,
         ]
     ]
 ];
--- config/settings.php
+++ config/settings.php
@@ -5,41 +5,56 @@ return ['Settings' => [
             'Mpay24.General' => [
                 'label' => __d('shop', 'Mpay24 configuration'),
             ],
+            'Mpay24.Production' => [
+                'label' => __d('shop', 'Mpay24 Live configuration'),
+            ],
+            'Mpay24.Testing' => [
+                'label' => __d('shop', 'Mpay24 Test configuration'),
+            ],
         ],
         'schema' => [
-            'Mpay24.useTestSystem' => [
-                'group' => 'Mpay24.General',
-                'type' => 'boolean',
-                'default' => false,
-            ],
-
+            // production
             'Mpay24.production.merchantId' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Production',
                 'type' => 'string',
                 'default' => '',
             ],
             'Mpay24.production.merchantPassword' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Production',
                 'type' => 'password',
                 'default' => '',
             ],
+//            'Mpay24.production.useTestSystem' => [
+//                'group' => 'Mpay24.Production',
+//                'type' => 'boolean',
+//                'default' => true,
+//                'help' => __('This MUST be UNCHECKED!')
+//            ],
             'Mpay24.production.debug' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Production',
                 'type' => 'boolean',
                 'default' => false,
             ],
+
+            // testing
             'Mpay24.testing.merchantId' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Testing',
                 'type' => 'string',
                 'default' => '',
             ],
             'Mpay24.testing.merchantPassword' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Testing',
                 'type' => 'password',
                 'default' => '',
             ],
+            'Mpay24.testing.useTestSystem' => [
+                'group' => 'Mpay24.Testing',
+                'type' => 'boolean',
+                'default' => false,
+                'help' => __('This MUST be CHECKED!')
+            ],
             'Mpay24.testing.debug' => [
-                'group' => 'Mpay24.General',
+                'group' => 'Mpay24.Testing',
                 'type' => 'boolean',
                 'default' => false,
             ],
